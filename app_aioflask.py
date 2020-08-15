779da48b760a8710aad14937cb36fa1311e4ba84
Miguel Grinberg <miguel.grinberg@gmail.com>
2020-08-16 00:30:03 +0100

    aioflask benchmark

---
+++ app_aioflask.py
@@ -0,0 +1,12 @@
+from os import environ
+
+from aioflask import Flask
+from async_db import get_row
+
+app = Flask("python-web-perf")
+
+
+@app.route("/test")
+async def test(request):
+    a, b = await get_row()
+    return {"a": str(a).zfill(10), "b": b}
